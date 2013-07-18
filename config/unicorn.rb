ae67681cda4a382038809a328c594cf126e0c4f0
Ross <rostikk@gmail.com>
2013-07-18 23:15:06 +0300

    added capistrano and unicorn test

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
-root = "/home/rails/current"
+root = "/home/root/apps/shop/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
 # change the YOUR_APP_NAME to your application name
-listen "/tmp/unicorn.shop-2.sock"
+listen "/tmp/unicorn.shop.sock"
 worker_processes 2
 timeout 30
 
