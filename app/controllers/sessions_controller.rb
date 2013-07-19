0afdc35d39d8217763896c1561b6f0cbd9d671d7
Ross <rostikk@gmail.com>
2013-07-20 00:04:04 +0300

    added capistrano and unicorn test

--- app/controllers/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class SessionsController < Devise::RegistrationController
-  skip_before_filter :require_no_authentication, :only => [:create, :new, :cancel]
-
-  def new
-    super
-  end
-end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,29 @@ stdout_path "#{root}/log/unicorn.log"
 listen "/tmp/unicorn.shop.sock"
 worker_processes 2
 timeout 30
+preload_app true
 
 before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = '/mnt/Hipstaweb-<%= RUBBER_ENV %>/current/Gemfile'
+  ENV['BUNDLE_GEMFILE'] = "#{root}/Gemfile"
+end
+
+before_fork do |server, worker|
+  # Перед тем, как создать первый рабочий процесс, мастер отсоединяется от базы.
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  # Ниже идет магия, связанная с 0 downtime deploy.
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # После того как рабочий процесс создан, он устанавливает соединение с базой.
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
 end
