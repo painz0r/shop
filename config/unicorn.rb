d06b4c373ac45938a3516bc98469c2bfd7a47872
Ross <rostikk@gmail.com>
2013-07-20 02:46:25 +0300

    added capistrano and unicorn test

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,3 +14,23 @@ before_exec do |server|
   ENV['BUNDLE_GEMFILE'] = "#{root}/Gemfile"
 end
 
+before_fork do |server, worker|
+  # Перед тем, как создать первый рабочий процесс, мастер отсоединяется от базы.
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  # Ниже идет магия, связанная с 0 downtime deploy.
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # После того как рабочий процесс создан, он устанавливает соединение с базой.
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+end
